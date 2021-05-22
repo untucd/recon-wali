ab84aded89eece963821289c7dfee51e47b540b4
untucd <84371758+untucd@users.noreply.github.com>
2021-05-22 18:22:43 +0530

    Init subdomain finding code

--- scripts/subdomain-finder.sh
+++ scripts/subdomain-finder.sh
@@ -1,1 +1,44 @@
+source ~/.bash_profile
 
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+BLUE=$(tput setaf 4)
+RESET=$(tput sgr0)
+
+echo "${RED} ######################################################### ${RESET}"
+echo "${RED} #                         Let's Hunt                    # ${RESET}"
+echo "${RED} ######################################################### ${RESET}"
+
+while getopts ":d:" input; do
+        case "$input" in
+        d)
+                domain=${OPTARG}
+                ;;
+        esac
+done
+if [ -z "$domain" ]; then
+        echo "${BLUE}Please give a domain like \"-d domain.com\"${RESET}"
+        exit 1
+fi
+
+mkdir $domain
+cd $domain
+
+echo "${BLUE} ######################################################### ${RESET}"
+echo "${BLUE} #                   Finding Subdomains                  # ${RESET}"
+echo "${BLUE} ######################################################### ${RESET}"
+
+sublist3r -d $domain -v -o op.txt
+subfinder -d $domain -o op.txt
+assetfinder --subs-only $domain | tee -a op.txt
+amass enum -passive -d $doamin | tee -a op.txt
+amass enum -active -d $domain -ip | tee -a amass_ips.txt
+cat amass_ips.txt | awk '{print $1}' | tee -a op.txt
+curl -s https://crt.sh/\?q\=$domain\&output\=json | jq &>/dev/null | awk -F'"' '/value/{print $4}' | sed 's/*.//g;s/\\n/\n/g' | sed 's/ /\n/g' | tee -a op.txt
+curl -s https://otx.alienvault.com/api/v1/indicators/domain/$domain/passive_dns | jq &>/dev/null | awk -F': ' '/"hostname": "(.*'$domain')"/{print $2}' | sed 's/[",]//g' | sed 's/ /\n/g' | tee -a op.txt
+curl -s https://dns.bufferover.run/dns\?q\=.$domain | awk -F',' '/.*'$domain'/{print $2}' | sed 's/"//g' | sed 's/ /\n/g' | tee -a op.txt
+curl -s http://api.hackertarget.com/hostsearch/\?q\=$domain | cut -d',' -f1 | sed 's/ /\n/g' | tee -a op.txt
+curl -s https://rapiddns.io/subdomain/$domain\?full\=1 | awk -F'">' '/'$domain'/{print $2}' | cut -d'<' -f1 | awk 'NF' | sed 's/ /\n/g' | tee -a op.txt
+curl -s https://api.sublist3r.com/search.php\?domain\=$domain | jq &>/dev/null | awk -F'"' '/'$domain'/{print $2}' | sed 's/ /\n/g' | tee -a op.txt
+curl -s https://www.threatcrowd.org/searchApi/v2/domain/report/\?domain\=$domain | jq &>/dev/null | awk -F'"' '/\.'$domain'/{print $2}' | sed 's/ //g' | sed 's/ /\n/g' | tee -a op.txt
+cat op.txt | sort -u | tee -a all.txt
