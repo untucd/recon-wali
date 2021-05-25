8546f145cf8cc5da0f944b75d742a8f9bf2b65fd
untucd <84371758+untucd@users.noreply.github.com>
2021-05-25 11:12:10 +0530

    updated altdns to terminate after 5hr

--- scripts/altdns-script.sh
+++ scripts/altdns-script.sh
@@ -25,7 +25,10 @@ echo "${GREEN} ######################################################### ${RESET
 echo "${GREEN} #                Subdomain Bruteforcing                 # ${RESET}"
 echo "${GREEN} ######################################################### ${RESET}"
 
-altdns -i output/$domain/all.txt -o output/$domain/data_output -w ~/tools/recon/patterns.txt -r -s output/$domain/results_output.txt -t 250
+
+(altdns -i output/$domain/all.txt -o output/$domain/data_output -w ~/tools/recon/patterns.txt -r -s output/$domain/results_output.txt -t 250) & pid=$!
+sleep 18000 && kill "$pid" && sleep 20
+
 mv output/$domain/results_output.txt output/$domain/dns_op.txt
 cat output/$domain/dns_op.txt > output/$domain/output.txt
 cat output/$domain/output.txt | awk -F ":" '{print $1}' | sort -u | tee -a output/$domain/all.txt
